f18a9e0ee7010bb0b81e62b1dca52357a0028e28
Navneel <navneel99@gmail.com>
2018-07-12 00:07:11 +0530

    getEmotions function added

--- js/processFace.js
+++ js/processFace.js
@@ -4,7 +4,51 @@ const uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/det
 
 function getEmotionScores(image) {
     // This function grabs an image, and makes query to the Microsoft's Face API
-    // It retrieves and returns various emotion scores
+    // It retrieves and returns various emotion
+    var emotion
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes":
+            "emotion"
+    };
+    var sourceImageUrl = url;
+    document.querySelector("#sourceImage").src = sourceImageUrl;  //It is displaying the image
+
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+        type: 'POST',
+        processData: false,
+        contentType: 'application/octet-stream',
+
+        // Request headers.
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        // Request body.
+        data: makeblob(sourceImageUrl),
+    })
+    .done(function (data) {
+        // Show formatted JSON on webpage.
+
+        lol=data[0]["faceAttributes"]["emotion"];
+        $("#responseTextArea").val(JSON.stringify(lol,null,2));
+        //$("#responseTextArea").val(JSON.stringify(data, null, 2));
+         emotion=JSON.stringify(lol);
+         return lol
+    })
+
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        // Display error message.
+        var errorString = (errorThrown === "") ?
+            "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ?
+            "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                jQuery.parseJSON(jqXHR.responseText).message :
+                jQuery.parseJSON(jqXHR.responseText).error.message;
+        alert(errorString);
+    });
 
 }
 
@@ -13,14 +57,15 @@ function getRating(emotionScores) {
 
 }
 
-
+/*
 function processImage() {
     var params = {
         "returnFaceId": "true",
         "returnFaceLandmarks": "false",
         "returnFaceAttributes":
-            "age,gender,headPose,smile,facialHair,glasses,emotion," +
-            "hair,makeup,occlusion,accessories,blur,exposure,noise"
+            "age,emotion"
+            //"age,gender,headPose,smile,facialHair,glasses,emotion," +
+            //"hair,makeup,occlusion,accessories,blur,exposure,noise"
     };
 
     // Display the image.
@@ -43,7 +88,10 @@ function processImage() {
     })
     .done(function (data) {
         // Show formatted JSON on webpage.
-        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        lol=data[0]["faceAttributes"]["emotion"];
+        $("#responseTextArea").val(JSON.stringify(lol));
+        emotion=JSON.stringify(data);
+
     })
 
     .fail(function (jqXHR, textStatus, errorThrown) {
@@ -57,3 +105,4 @@ function processImage() {
         alert(errorString);
     });
 }
+*/
