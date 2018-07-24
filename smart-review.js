6c911e09b30aa71732ed364e420ba7b48e492bbd
ArpanMangal <mangalarpan@gmail.com>
2018-07-24 16:22:25 +0530

    Polymer Dialog added

--- smart-review.js
+++ smart-review.js
@@ -4,6 +4,10 @@
 // Import the PolymerElement base class and html helper
 import {PolymerElement, html} from '@polymer/polymer';
 
+// Importing Elements
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-dialog/paper-dialog.js';
+
 // Define an element class
 class SmartReview extends PolymerElement {
 
@@ -15,9 +19,31 @@ class SmartReview extends PolymerElement {
     return html`
       <style>
         .response { margin-top: 10px; } 
+
+        paper-dialog.colored {
+          border: 2px solid;
+          border-color: #4caf50;
+          background-color: #f1f8e9;
+          color: #4caf50;
+        }
+
+        paper-dialog.size {
+          height: 400px;
+          width: 600px;
+        }
       </style>
-      <h1> hi </h1>
+      <h1> hid </h1>
+
+      <paper-button raised class="green" on-tap="openDialog"><slot></slot></paper-button>
+      <paper-dialog id="reviewDialog" class="colored size">
+        <h2 style="text-align: center">Smart Review</h2>
+        <p>Cool</p>
+      </paper-dialog>
     `;
+  }
+
+  openDialog () {
+    this.$.reviewDialog.open();
   }
 }
 
